b26a90d730db19c152b7ac7c314aebbca037c2b7
pp1985paw <65330065+pp1985paw@users.noreply.github.com>
2020-05-14 00:23:38 -0700

    Update HomeServlet.java

--- src/com/abacusdev/servlet/HomeServlet.java
+++ src/com/abacusdev/servlet/HomeServlet.java
@@ -25,7 +25,7 @@ public class HomeServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Inside Home");
 		request.getRequestDispatcher("JSP/AbacusStart.jsp").forward(request, response);
